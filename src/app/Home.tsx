77d9cc397cd4fbab3c0560f1620aa90e475a8411
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-06-07 19:38:14 -0300

    feat: bottom navigator

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,15 +1,16 @@
 import { ButtonIcon } from "@/components/ButtonIcon";
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
-import { StackRoutesProps } from "@/routes/StackRoutes";
+// import { StackRoutesProps } from "@/routes/StackRoutes";
+import { BottomRoutesProps } from "@/routes/BottomRoutes";
 import { View } from "react-native";
 
-export function Home({ navigation }: StackRoutesProps<"home">) {    
+export function Home({ navigation }: BottomRoutesProps<"home">) {    
     return (
         <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
             <Header>
                 <Title>Home</Title>
-                <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product", { id: "320" })} />
+                <ButtonIcon name="add-circle" onPress={() => navigation.navigate("product", { id: "1" })} />
             </Header>
         </View>
     )
